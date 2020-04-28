51d008d29c785998e4fea77179988850f28bde6a
Luis Felipe Colosimo <luisfelipecolosimo@gmail.com>
2020-04-27 21:44:10 -0300

    set routes

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,17 @@ const { celebrate, Segments, Joi } = require('celebrate');
 
 const routes = express.Router();
 
-routes.get('/session', (req,res) =>{
-  console.log('ola foi');
-  return res.status(400).json({ error: 'no ONG found with this ID' })
-});
+routes.get('/session', (req,res) =>{});
+
+
+routes.post('/user', (req,res) =>{});
+routes.get('/user', (req,res) =>{});
+//routes.delete('/user', (req,res) =>{});
+
+routes.get('/profile', (req,res) =>{});
+
+routes.post('/incidents', (req,res) =>{});
+routes.get('/incidents', (req,res) =>{});
+routes.delete('/incidents/:id', (req,res) =>{});
 
 module.exports = routes;
